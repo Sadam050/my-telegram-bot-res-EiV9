5d5a451e9cfbab8e8488ed6342beb5197ae28c36
Sadam050 <129305750+Sadam050@users.noreply.github.com>
2025-08-01 16:48:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ async def start(update, context):
     await update.message.reply_text("🤖 Bot is online!")
 
 if __name__ == "__main__":
-    app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
+    app = Application.builder().token(os.getenv("8211462719:AAG-E5gWUo_gGa6lorCn1byph5oR8Q3IVS8")).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
